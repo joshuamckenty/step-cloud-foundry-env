c236d26ee0b0f76875f67608b51e0d6e3130d571
Joshua McKenty <joshua@pistoncloud.com>
2014-08-05 18:08:58 -0700

    Gotta log in to set env.

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 key=${WERCKER_CLOUD_FOUNDRY_ENV_KEYNAME}
 appname=${WERCKER_CLOUD_FOUNDRY_ENV_APPNAME}
+username=$WERCKER_CLOUD_FOUNDRY_DEPLOY_USERNAME
+password=$WERCKER_CLOUD_FOUNDRY_DEPLOY_PASSWORD
+organization=$WERCKER_CLOUD_FOUNDRY_DEPLOY_ORGANIZATION
+space=${WERCKER_CLOUD_FOUNDRY_DEPLOY_SPACE-development}
+cf login -u $username -p $password -o $organization -s $space
 cf set-env $appname $key ${!key}
